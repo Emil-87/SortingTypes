fa01b2f768ea43181e55852c3a5b209bc6f476a5
Emil-87 <emil.pegas@gmail.com>
2023-03-12 02:46:02 +0600

    Сортировка подсчетом на отрицательных числах

--- CountingSort/Program.cs
+++ CountingSort/Program.cs
@@ -1,16 +1,17 @@
 ﻿// Сортировка подсчетом
 
-int[] array = { 0, 2, 3, 2, 1, };
+int[] array = { -10, -5, -9, 0, 2, 5, 1, 3, 10, 1 };
+int[] sortedArray = CountingSortExtended(array);
 
-CountingSort(array);
+//CountingSort(array);
 
-Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", sortedArray));
 
 void CountingSort(int[] inputArray)
 {
     int[] counters = new int[10];
 
-    int ourNumber;
+    //int ourNumber;
 
     for (int i = 0; i < inputArray.Length; i++)
     {
@@ -30,3 +31,30 @@ void CountingSort(int[] inputArray)
     }
 }
 
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+    Console.WriteLine(max + offset + 1);
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+
+    return sortedArray;
+}
